ca67c848c3fd448ccbd363f988003f8bda0c2fb5
MagisterLysaGlowa <kacperpiaskowy937@gmail.com>
2024-10-23 08:52:29 +0200

    Add read albums method

---
+++ konsola/MusicAlbum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola
+{
+    public class MusicAlbum
+    {
+        public string? Artist { get; set; }
+        public string? Album { get; set; }
+        public uint SongsNumber { get; set; }
+        public int YearOfEdition { get; set; }
+        public uint DownloadNumber { get; set; }
+
+        public MusicAlbum(string? artist, string? album, uint songsNumber, int yearOfEdition, uint downloadNumber)
+        {
+            Artist = artist;
+            Album = album;
+            SongsNumber = songsNumber;
+            YearOfEdition = yearOfEdition;
+            DownloadNumber = downloadNumber;
+        }
+    }
+}
--- konsola/Program.cs
+++ konsola/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace konsola
+﻿using System.Collections.Generic;
+
+namespace konsola
 {
-    internal class Program
+    public class Program
     {
+        public static List<MusicAlbum> ReadAlbums(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            List<MusicAlbum> albums = new();
+            for (int i = 0; i < lines.Length; i+=6)
+            {
+                string artist = lines[i];
+                string album = lines[i + 1];
+                uint songsNumber = uint.Parse(lines[i + 2]);
+                int yearOfEdition = int.Parse(lines[i + 3]);
+                uint downloadNumber = uint.Parse(lines[i + 4]);
+                albums.Add(new MusicAlbum(artist, album, songsNumber, yearOfEdition, downloadNumber));
+            }
+            return albums;
+        }
+
+        private static void DisplayAlbums(List<MusicAlbum> albums)
+        {
+            foreach (var item in albums)
+            {
+                Console.WriteLine(item.Artist);
+                Console.WriteLine(item.Album);
+                Console.WriteLine(item.SongsNumber);
+                Console.WriteLine(item.YearOfEdition);
+                Console.WriteLine(item.DownloadNumber);
+                Console.WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filepath = "../../../Data.txt";
+            //DisplayAlbums(ReadAlbums(filepath));
         }
     }
 }
